4fad1a72dd4396c49a224143207cd4baaafe5a20
armagidon123 <v.maxn@mail.ru>
2023-12-16 13:16:22 +0500

    FirstCommit
    PythonApp(mp4 -> mp3)

---
+++ main.py
@@ -0,0 +1,7 @@
+import moviepy.editor
+from pathlib import Path
+video_file = Path('Название_файла_видео')
+
+video = moviepy.editor.VideoFileClip(f'{video_file}')
+audio = video.audio
+audio.write_audiofile(f'{video_file.stem}.mp3')
